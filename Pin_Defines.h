fba0570be90c89800cf3919bab4197fcc8aaf2a8
Bobbbl <sebastian.draxinger@googlemail.com>
2018-12-26 18:46:53 +0100

    Fucked Up With Script

---
+++ Pin_Defines.h
@@ -0,0 +1,22 @@
+#ifndef PiN_DEFINES
+#define PiN_DEFINES
+
+#include <Arduino.h>
+#define DEBUG
+
+#define PWM1 5
+#define PWM2 6
+#define DIR1 24
+#define DIR2 23
+#define END1 4
+#define END2 3
+#define RENC_A 2
+#define RENC_B 3
+#define CS A1
+#define CD A0
+#define MOSI 51
+#define MISO 50 
+#define CLK 52
+
+
+#endif
