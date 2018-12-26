43f18769d5ba3ffe515792cd38c876022cfa714f
Bobbbl <sebastian.draxinger@googlemail.com>
2018-12-26 18:56:41 +0100

    testagain

---
+++ Marvin_Communication.h
@@ -0,0 +1,53 @@
+#ifndef MARVIN_COMMUNICATION
+#define MARVIN_COMMUNICATION
+
+#include <Arduino.h>
+#include <string.h>
+#include "Pin_Defines.h"
+#include "Marvin_Motor.h"
+
+#define dict_length 20
+#define MESSAGE_TIMEOUT (unsigned long)5000  // Timeout in ms
+
+using namespace std;
+
+typedef enum {Start_Session = 0, 
+End_Session = 1,
+Start_Homing = 2,
+Send_Toolpath = 3,
+Empfang_Bestaetigt = 4,
+Receive_Error = 5,
+Receive_Successfull = 6,
+Point_Reached = 7,
+No_Message = -1
+}communication_alphabet;
+
+#define COMM_LENGTH 8
+
+String comm_dict[COMM_LENGTH]= {
+    "__Start_Session__",
+    "__End_Session__",
+    "__Start_Homing__",
+    "__Send_Toolpath__",
+    "__Empfang_Besteatigt__",
+    "__Receive_Error__",
+    "__Receive_Successfull__",
+    "__Point_Reached__"
+};
+
+
+
+uint8_t checkConnection();
+communication_alphabet checkForValidMessage();
+communication_alphabet waitForSession();
+communication_alphabet waitForKonsekutiveMessage();
+Strecke readToolPathLine();
+Strecke receiveKoordinate();
+Strecke receivePoint();
+
+// Communication shortcuts
+inline void sendNACK();
+inline void sendEndSession();
+inline void sendError();
+inline void sendReceiveSuccessfull();
+#endif
---
+++ Marvin_Motor.h
@@ -0,0 +1,143 @@
+#ifndef MARVIN_MOTOR
+#define MARVIN_MOTOR
+
+#include <Arduino.h>
+#include "Pin_Defines.h"
+#include <math.h>
+#include <string.h>
+
+
+extern uint8_t endschalter_flag_x;
+extern uint8_t endschalter_flag_y;
+
+#define STEPS_PER_REVOLUTION_X 25600
+#define STEPS_PER_REVOLUTION_Y 25600
+#define DISTANCE_PER_REVOLUTION_X 0.5 // Strecke pro Umdrehung in mm
+#define DISTANCE_PER_REVOLUTION_Y 0.5 // Strecke pro Umdrehung in mm
+
+#define STEPS_PER_MILLIMETER_X (float)400.00  // Wieviele Schritte ergeben einen Millimeter X
+#define STEPS_PER_MILLIMETER_Y (float)400.00  // Vieviele Schritte ergeben einen Millimeter Y
+
+#define CPU_FREQ (unsigned long)16000000
+
+typedef enum {
+    links = 0,
+    rechts = 1
+}Richtung;
+
+typedef struct {
+    float x;
+    float y;
+    float f;
+
+    uint8_t error;
+    uint8_t end_session;
+}Strecke;
+
+typedef struct {
+    int steps_x;
+    int steps_y;
+    float rpm_x;
+    float rpm_y;
+
+    uint8_t error;
+    uint8_t end_session;
+}Strecke_Steps_RPM;
+
+typedef struct {
+  float x;
+  float y;
+}Point;
+
+typedef struct {
+  float x;
+  float y;
+}Vector;
+
+Strecke_Steps_RPM convertToStepsAndRPM(Strecke s);
+
+
+class Marvin_Steppers{
+
+public:
+  Marvin_Steppers(unsigned int PWM_x, unsigned int PWM_y, unsigned int DIR_x, unsigned int DIR_y) : pwm_x(PWM_x),
+                                                                                                    pwm_y(PWM_y),
+                                                                                                    dir_x(DIR_x),
+                                                                                                    dir_y(DIR_y)
+  {
+    this->number_of_steps_x = 200;
+    this->number_of_steps_y = 200;
+    // Timer is Timer 3 - OC3A is Pin 6 PWM
+    // Disable all Interrupts
+    noInterrupts();
+    TCCR3A = 0;
+    TCCR3B = 0;
+    TCNT3 = 0;
+    TCCR3A = 0;
+    TCCR3B = 0;
+    TCCR4A = 0;
+    TCCR4B = 0;
+    // Waveform Generation Mode
+    TCCR3A |= (1 << WGM32); // CTC Mode
+    // Compare Output Mode
+    // TCCR3A |= (1 << COM3A0);  // Toggle OC3A (PE3) bzw. PWM 6 on Compare Match
+    // TCCR3A &= ~(1 << COM3A1); // Toggle OC3A (PE3) bzw. PWM 6 on Compare Match
+    // Prescaler
+    this->prescaler = 64;
+    // TCCR3B |= (1 << CS31); // Prescaler 8
+    // Interrupts
+    TIMSK3 |= (1 << OCIE3A); // Output Compare Interrupt Enabled
+    OCR3A = 0;
+
+    TCCR4A = 0;
+    TCCR4B = 0;
+    TCNT4 = 0;
+    // Waveform Generation Mode
+    TCCR4A |= (1 << WGM42); // CTC Mode
+    // Compare Output Mode
+    // TCCR4A |= (1 << COM4A0);  // Toggle OC3A (PE3) bzw. PWM 6 on Compare Match
+    // TCCR4A &= ~(1 << COM4A1); // Toggle OC3A (PE3) bzw. PWM 6 on Compare Match
+    // Prescaler
+    this->prescaler = 64;
+     //TCCR4B |= (1 << CS41); // Prescaler 8
+    // Interrupts
+    TIMSK4 |= (1 << OCIE4A); // Output Compare Interrupt Enabled
+    OCR4A = 0;
+    interrupts();
+  }
+
+  void stepPWM(Strecke_Steps_RPM s);
+  void easyStep(Strecke s);
+  void setSpeed_x(long whatSpeed);
+  void setSpeed_y(long whatSpeed);
+  void step(int steps_to_move_x, int steps_to_move_y);
+  void stepMotorX(Richtung r);
+  void stepMotorY(Richtung r);
+  void checkLimitSwitches();
+  void doHoming();
+  uint8_t x_flag;
+  uint8_t y_flag;
+  uint16_t prescaler;
+  int number_of_steps_x, number_of_steps_y; // total number of steps per revolution
+  void startTimer3(unsigned long prescaler);
+  void stopTimer3();
+  void startTimer4(unsigned long prescaler);
+  void stopTimer4();
+  void setDirectionMotorX(char *str);
+  void setDirectionMotorY(char *str);
+void bresenham(Strecke s);
+void tt(Strecke s);
+  int version();
+
+private:
+  void stopTimer();
+  int direction_x, direction_y;
+  unsigned long step_delay_x, step_delay_y; // delay between steps, in ms
+  int step_number_x, step_number_y;
+  unsigned long last_step_time_x, last_step_time_y;
+  int pwm_x, pwm_y;
+  int dir_x, dir_y;
+};
+
+
+#endif
